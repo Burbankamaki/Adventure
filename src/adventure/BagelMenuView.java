6893599e5a3bbc0c15ab7daf4fe5dc5a5c3ac21b
page-lisa <lisapage@10.18.150.73>
2013-11-08 12:02:12 -0700

    commit with errors old file to pull

---
+++ src/adventure/BagelMenuView.java
@@ -0,0 +1,145 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adventure;
+
+/**
+ *
+ * @author lisapage
+ */import java.util.Scanner;
+
+public class BagelMenuView {
+    
+    int i;
+    String selection;
+    BagelMenuControl customer = new BagelMenuControl();
+    public double max;
+    
+      
+    
+    public final static String[][] menuItems = {
+        {"1", "Blueberry Bagel","2.95"},
+        {"2", "Strawberry Bagel", "2.95"}, 
+        {"3", "Everything Bagel", "2.95"},
+        {"4", "Poppyseed Bagel", "2.95"},
+        {"5", "Onion Bagel", "2.95"},
+        {"6", "Cheesey Pizza Bagel", "4.95"},
+        {"7", "Ham and Swiss Bagel", "4.95"},
+        {"8", "Roast Beef and Cheddar Bagel", "4.95"},
+    };
+            
+     public BagelMenuView() {
+         
+    } 
+    
+    public void displayMenu(){
+        BagelMenuView customer = new BagelMenuView();
+         System.out.println("Welcome to The Hole Bagel Shop!\n"
+                 +"Please choose up to four items off the menu by entering the \n" 
+                 +"number associated with the item you wish to purchase. Enter \"q\"\n"
+                 +"at any time to finish ordering");
+         for (i=0;i<5;i++){
+         System.out.println(menuItems[i][0] + menuItems[i][1] + menuItems[i][2]);
+     
+         }
+    }    
+    public void getInput(){
+        
+        do {
+             this.displayMenu();
+             selection = this.getSelection();
+             //gets validated selection value
+             Scanner input = new Scanner(System.in);
+             switch (selection){
+                 
+                 case "1":
+                     
+                    customer.recordPurchase(String[][] menuItems[0][1], menuItems[0][2]);
+                     break;
+                     
+                 case "2":
+                     
+                     
+                     break;
+                     
+                 case "3":
+                     
+                     
+                     break;
+                     
+                 case "4":
+                     
+                     
+                     break;
+                      
+                 case "5":
+                     
+                     
+                     break;
+                     
+                 case "6":
+                     
+                     
+                     break;
+                     
+                 case "7":
+                     
+                     
+                     break;
+                     
+                 case "8":
+                     
+                     
+                     break; 
+             }
+             }while (!selection.equals("q"));
+                    
+        }     
+               
+    protected final String getSelection() {
+              
+            String choice;
+            Scanner input = new Scanner(System.in);
+        boolean valid = false;
+        do {
+
+            choice = input.next();
+            valid = validCommand(choice);
+            if (!validCommand(choice)) {
+                System.out.println("Invalid command. Please enter a valid command.");
+                continue;
+            }
+                
+        } while (!valid);
+        
+        return choice;
+    }
+    
+      private boolean validCommand(String selection) {
+        String[][] items = BagelMenuView.menuItems;
+
+        for (String[] item : BagelMenuView.menuItems) {
+            if (item[0].equals(selection)) {
+                return true;
+            }
+        }
+        return false;
+      }
+      
+      public void displayMax(){
+    System.out.println("The most expensive item on your list is $" + max);
+    }
+}  
+    //write display function
+     //write get Input function and get value
+     //Array List of bagels purchased--to use several times needs to be an instance variable declared at the top
+     //add the item selected to list  
+     //Calculate the amount of purchase
+     //Display receipt
+     //calculate Max compare
+     
+    
+     
+     
+            

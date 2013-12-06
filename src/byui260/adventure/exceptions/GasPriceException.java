4e10e9732f5817c64a5c2955d706e9c98e56c825
page-lisa <lisapage@10.18.150.73>
2013-12-06 12:58:49 -0700

    Finished Try Catch Finally in Main Class

---
+++ src/byui260/adventure/exceptions/GasPriceException.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui260.adventure.exceptions;
+
+/**
+ *
+ * @author Motoko Kusanagi
+ */
+public class GasPriceException extends Exception{
+ 
+    
+    public GasPriceException() {
+    }
+
+    public GasPriceException(String message) {
+        super(message);
+    }
+
+    public GasPriceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public GasPriceException(Throwable cause) {
+        super(cause);
+    } 
+    
+    
+    
+    
+    
+    
+    
+}
